9be810de8789092d383d40caa1e73c53a31c866f
hyunzero <garden940316@gmail.com>
2024-05-21 00:46:35 +0900

    programmers sort(k번째수)

---
+++ src/programmers/sort/Solution.java
@@ -0,0 +1,27 @@
+package programmers.sort;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+// 정렬 - K번째수 풀이1
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for(int q = 0; q < commands.length; q++) {
+            int[] tmp = commands[q];
+            ArrayList<Integer> list = new ArrayList<>();
+
+            int i = tmp[0] - 1;
+            int j = tmp[1] - 1;
+            int k = tmp[2] - 1;
+
+            for(int x = i; x <= j; x++) {
+                list.add(array[x]);
+            }
+            Collections.sort(list);
+            answer[q] = list.get(k);
+        }
+        return answer;
+    }
+}
---
+++ src/programmers/sort/Solution2.java
@@ -0,0 +1,27 @@
+package programmers.sort;
+
+import java.util.Arrays;
+
+//정렬 - K번째수 풀이2
+class Solution2 {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for(int q = 0; q < commands.length; q++) {
+            int[] comArr = commands[q];
+            
+            int i = comArr[0] - 1;
+            int j = comArr[1] - 1;
+            int k = comArr[2] - 1;
+            
+            int[] tmpArr = new int[j - i + 1];
+            int idx = 0;
+            for(int x = i; x <= j; x++) {
+                tmpArr[idx++] = array[x];
+            }
+            Arrays.sort(tmpArr);
+            answer[q] = tmpArr[k];
+        }
+        return answer;
+    }
+}
